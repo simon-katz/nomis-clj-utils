dcca6d7c276e1e84c082d7fd4b68e341d0651ccc
Simon Katz <git@nomistech.com>
2013-05-09 18:06:04 +0100

    Fix incorrect test description.

--- test/com/nomistech/clj_utils_test.clj
+++ test/com/nomistech/clj_utils_test.clj
@@ -5,5 +5,5 @@
 (fact "`member?` tests"
       (fact "Returns truthy if the item is in the collection"
             (member? :b [:a :b :c]) => truthy)
-      (fact "Returns falsey if the item is in the collection"
+      (fact "Returns falsey if the item is not in the collection"
             (member? :d [:a :b :c]) => falsey))
